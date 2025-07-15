f2516123361d4acd1994dcdfec20400e9a33e0bc
veeravel-iyappan-49 <velveeravel4339@gmail.com>
2025-07-15 15:33:01 +0530

    delete added

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import CourseList from "./CourseList";
 
 function Course(props) {
+  function BuyCourse(discount) {
+    console.log(
+      props.name + " full course purchased with " + discount + "% discount"
+    );
+  }
   return (
-    // using conditional rendering for omit the empty or unnamed entries
+    // using conditional rendering to omit the empty or unnamed entries
+
     props.name && (
       <div className="card">
         <img src={props.image} alt="Course Image" />
         <h3>{props.name} Full Course</h3>
         <p>Duration:{props.time}</p>
         <p>
-          Price:<b>{props.price}</b>
+          <mark>
+            Price:<b>{props.price}</b>
+          </mark>
         </p>
         <p>Rating:{props.rating}</p>
+        <button onClick={() => BuyCourse(20)}>Buy Now</button>
+        <button onClick={()=>props.delete(props.id)}>Delete</button>
       </div>
     )
   );
--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HTML from "./assets/html.jpeg";
 import CSS from "./assets/css.jpeg";
 import JS from "./assets/js.jpeg";
@@ -6,7 +6,7 @@ import react from "./assets/react.jpeg";
 import Course from "./course";
 
 function CourseList() {
-  const courses = [
+  const [courses, setCourse] = useState([
     {
       id: 1,
       image: HTML,
@@ -48,19 +48,26 @@ function CourseList() {
       price: 599,
       rating: "9/10",
     },
-  ];
+  ]);
+
+  function handleDelete(id) {
+    const newCourses = courses.filter((course) => course.id != id);
+    setCourse(newCourses);
+  }
 
   //for sorting
-  courses.sort((x, y) => y.price - x.price);
+  //   courses.sort((x, y) => y.price - x.price);
 
-  const courseList = courses.map((course, index) => (
+  const courseList = courses.map((course) => (
     <Course
-      key={index}
+      key={course.id}
       name={course.name}
       image={course.image}
       rating={course.rating}
       price={course.price}
       time={course.time}
+      delete={handleDelete}
+      id={course.id}
     />
   ));
 
